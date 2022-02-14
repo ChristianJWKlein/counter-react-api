0566f700de654ce34d1ae4afdfae94f381016173
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-02-14 11:47:22 -0500

    coffees list iced or hot

--- src/Coffees.js
+++ src/Coffees.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function Coffees() {
   const [coffeeList, setCoffeeList] = useState();
+  const [temperature, setTemperature] = useState("hot");
   useEffect(() => {
-    fetch("https://api.sampleapis.com/coffee/hot")
+    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
       .then((response) => response.json())
       .then((data) => setCoffeeList(data))
       .catch((err) => console.error(err));
-  }, []);
+  }, [temperature]);
   return (
     <section className="coffee-container" id="coffees">
+      {/* We need our JSX to be inside a SINGLE CONTAINING TAG. That tag can be a React.Fragment <></> if we don't want any HTML to render */}
       <h2>Coffee Recipes</h2>
+      <button onClick={() => setTemperature("hot")}>Hot</button>
+      <button onClick={() => setTemperature("iced")}>Iced</button>
       {!coffeeList ? (
         <h3>Loading...</h3>
       ) : (
