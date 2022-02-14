1792bf52239dadabd901d44bbd481f935272e8e1
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-02-14 11:28:26 -0500

    coffees list action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Coffees from "./Coffees.js";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    setCounter(500);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +16,8 @@ function App() {
         &nbsp;
         <button onClick={() => setCounter(counter - 1)}> -</button>
         &nbsp;
-        <button onClick={() => setCounter(0)}> reset</button>
+        <button onClick={() => setCounter(0)}> Reset</button>
+        <Coffees />
       </header>
     </div>
   );
